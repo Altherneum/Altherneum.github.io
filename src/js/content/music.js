119c98d9724343a34d95653bf0b195f0a7e65b1b
lx78WyY0J5 <9j2k37st@gmail.com>
2024-08-12 14:05:00 +0200

    added songs & pre make music update

--- src/js/content/music.js
+++ src/js/content/music.js
@@ -1,3 +1,16 @@
+const links = [
+    {
+        href: "/discord.html",
+        text: "Serveur Discord",
+        playlist: false,
+    },
+    {
+        href: "https://Altherneum.fr",
+        text: "Serveur Minecraft",
+        playlist: false,
+    },
+];
+
 function show(music) {
     hideAll();
 
@@ -12,4 +25,12 @@ function hide(music) {
 
 function hideAll() {
     hide("topvid"); hide("rain"); hide("chill"); hide("chilljp"); hide("classic"); hide("histoire"); hide("phonk"); hide("rap"); hide("trool");
+}
+
+function addMusics() {
+    //Ajouter toutes les musiques
+    // for loop de la hash map
+    // Ajouter dans la bonne catégorie
+    // fetch l'image de la cover
+    // Ajouter dans l'hash map un mode playlist
 }
