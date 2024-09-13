8fb114f10ad5ae369e6c825f132a7e852be8ea74
lx78WyY0J5 <9j2k37st@gmail.com>
2024-09-13 17:25:32 +0200

    add remote-ssh md file to linux page

--- src/js/include/index.js
+++ src/js/include/index.js
@@ -385,6 +385,7 @@ async function pageCours() {
             await addMarkdown('Altherneum/.github', 'note/OS/Linux/process.md', false, false);
 
             await addMarkdown('Altherneum/.github', 'note/OS/Linux/remote.md', false, false);
+            await addMarkdown('Altherneum/.github', 'note/OS/Linux/remote-ssh.md', false, false);
             await addMarkdown('Altherneum/.github', 'note/OS/Linux/UFW.md', false, false);
 
             await addMarkdown('Altherneum/.github', 'note/OS/Linux/HTTPD-from-src.md', false, false);
