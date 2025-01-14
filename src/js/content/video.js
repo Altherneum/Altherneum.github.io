467a8073d479455abd2d29feca5acfd5f9c4fa00
lx78WyY0J5 <9j2k37st@gmail.com>
2025-01-14 19:04:49 +0100

    game videos

--- src/js/content/video.js
+++ src/js/content/video.js
@@ -16,8 +16,32 @@ var videolinks = [
         videoID: "bVmukCdg7SQ",
         categorie: "IT",
     },
+    {
+        videoID: "_26Fg09tW_0",
+        categorie: "game",
+    },
+    {
+        videoID: "yle2LScFdWs",
+        categorie: "game",
+    },
+    {
+        videoID: "s8MT-5X7cyk",
+        categorie: "game",
+    },
+    {
+        videoID: "D_pw841ZR1E",
+        categorie: "game",
+    },
+    {
+        videoID: "RjEWyYs6zpw",
+        categorie: "game",
+    },
+    {
+        videoID: "qQcHkMvsQwA",
+        categorie: "game",
+    },
 ];
 
-const videoTypes = ["hack", "IT", "discord", "code", "malware"];
+const videoTypes = ["hack", "IT", "discord", "code", "malware", "game"];
 
 GetVideos(videolinks);
