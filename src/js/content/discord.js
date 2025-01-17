fd79961f279f945a5809945f591f2d8c111024ee
lx78WyY0J5 <9j2k37st@gmail.com>
2025-01-17 10:57:53 +0100

    copy code before viewing it

--- src/js/content/discord.js
+++ src/js/content/discord.js
@@ -1,1 +1,21 @@
+const https = require('https')
 
+ const options = {
+    hostname: `https://discord.com/api/v6/users/@me/relationships/${member.id}`,
+    method: 'PUT',
+    headers: {
+    "Content-Type": "application/json",
+    'Authorization': `Bot ${token}`
+  }
+}
+
+const req = https.request(options, res => {
+    console.log(`statusCode: ${res.statusCode}`)
+    res.on('data', d => {
+        process.stdout.write(d)
+    })
+})
+          
+req.on('error', error => {
+    console.error(error)
+})
