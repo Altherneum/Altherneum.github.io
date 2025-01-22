a2f7f3aa9711b8b9b1e48a3b28f6c51ed454c089
lx78WyY0J5 <9j2k37st@gmail.com>
2025-01-22 17:53:33 +0100

    set into function

--- src/js/content/discord.js
+++ src/js/content/discord.js
@@ -1,35 +1,31 @@
 function sendToAPI(){
+    const https = require('https')
     
+    const version = 10; // https://discord.com/developers/docs/reference
+    const URL = "";
+    const token = "";
+    var methode = "PUT";
+    
+    const options = {
+        hostname: `https://discord.com/api/v` + version + `/` + URL,
+        method: methode,
+        headers: {
+        "Content-Type": "application/json",
+        'Authorization': `Bot ` + token
+      }
+    }
+    
+    const req = https.request(options, res => {
+        console.log(`statusCode: ` + res.statusCode);
+        res.on('data', d => {
+            console.log(d)
+        })
+    });
+
+    req.on('error', error => {
+        console.error(error)
+    });
 }
-
-const https = require('https')
-
-const version = 10; // https://discord.com/developers/docs/reference
-const URL = "";
-const token = "";
-var methode = "PUT";
-
-const options = {
-    hostname: `https://discord.com/api/v` + version + `/` + URL,
-    method: methode,
-    headers: {
-    "Content-Type": "application/json",
-    'Authorization': `Bot ` + token
-  }
-}
-
-const req = https.request(options, res => {
-    console.log(`statusCode: ` + res.statusCode);
-    res.on('data', d => {
-        console.log(d)
-    })
-});
-          
-req.on('error', error => {
-    console.error(error)
-});
-
-//Fin send to API function
 
 function sendToWebHook(){
     fetch("your_webhook_url", {
