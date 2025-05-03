278a7bb2ec3e52324bde23104f9167d76691d43a
lx78WyY0J5 <9j2k37st@gmail.com>
2025-05-03 16:49:14 +0200

    remove useless comment

--- src/js/content/github-update.js
+++ src/js/content/github-update.js
@@ -27,7 +27,7 @@ async function getUpdate(repoName) {
     document.getElementById("author").textContent = "👤 Par : " + name;
 
     var date = getValue(z2, "date");
-    const date1 = new Date(Date.parse(date)); //Break date convert as it's a NEW date
+    const date1 = new Date(Date.parse(date));
     var date2 = date1.toLocaleString();
     document.getElementById("date").textContent = "⏰ " + date2;
 
