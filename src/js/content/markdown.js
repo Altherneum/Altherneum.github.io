9b5c8d6a731dacbf2ac164c5a1158f0d50ab0433
lx78WyY0J5 <9j2k37st@gmail.com>
2025-05-03 19:02:40 +0200

    avoid textarea & codeblock for markdown

--- src/js/content/markdown.js
+++ src/js/content/markdown.js
@@ -106,8 +106,8 @@ const parseMarkdown = async (text) => {
     toHTML = toHTML.replace(/!\[([^\[]+)\]\(([^\)]+)\)/g, '<img src=\"$2\" alt=\"$1\" />') // <img>
 
     toHTML = toHTML.replace(/\`{3}(.*?)\`{3}/gms, '<textarea>$1</textarea>') // <code>
-    toHTML = toHTML.replace(/(?<!<textarea>)\`{2} (.*?) \`{2}/gm, '<code>$1</code>') // backtick inside <code>
-    toHTML = toHTML.replace(/(?<!<textarea>)\`{1,2}(.*?)\`{1,2}/gm, '<code>$1</code>') // <code>
+    toHTML = toHTML.replace(/(?<!<(?:textarea|code)>)\`{2} (.*?) \`{2}/gm, '<code>$1</code>') // backtick inside <code>
+    toHTML = toHTML.replace(/(?<!<(?:textarea|code)>)\`{1,2}(.*?)\`{1,2}/gm, '<code>$1</code>') // <code>
 
     toHTML = toHTML.replace(/(?<!.)(-{3,})(?!.)/g, '<hr/>') //hr (Decoration line)
 
@@ -115,29 +115,31 @@ const parseMarkdown = async (text) => {
     toHTML = toHTML.replace(/\n(?:&gt;|\>)\W*(.*)/gim, '<blockquote><p>$1</p></blockquote>') // <blockquote>
 
     toHTML = toHTML.replace(/\_\_(.*?)\_\_/g, '<u>$1</u>') // underline
-    toHTML = toHTML.replace(/(?<!\\)(?<!\/)\*\*(.*?)\*\*/g, '<b>$1</b>') //bold
-    toHTML = toHTML.replace(/^(?<!\`)(?<!\\)(?<!\/)\*(.*?)\*/gm, '<i>$1</i>') //italic
+    toHTML = toHTML.replace(/(?<!<(?:textarea|code)[^>]*>[^<]*)(?<!\\)(\*{2}(.*?)\*{2})(?![^<]*<\/(?:textarea|code)>)/gim, '<b>$2</b>') //bold
+    toHTML = toHTML.replace(/(?<!<(?:textarea|code)[^>]*>[^<]*)(?<!\\)(\*{1}(.*?)(?!\\)\*{1})(?![^<]*<\/(?:textarea|code)>)/gim, '<i>$2</i>') //italic
 
-    toHTML = toHTML.replace(/^\*(.*$)/gim, '<ul><li>$1</li></ul>') // <li>
-    toHTML = toHTML.replace(/^- (.*$)/gim, '<ul><li>$1</li></ul>') // <li>
-    toHTML = toHTML.replace(/^ {2}- (.*$)/gim, '<ul><li style="margin-left:12px;">$1</li></ul>') // <li>
-    toHTML = toHTML.replace(/^ {4}- (.*$)/gim, '<ul><li style="margin-left:24px;">$1</li></ul>') // <li>
-    toHTML = toHTML.replace(/^ {6}- (.*$)/gim, '<ul><li style="margin-left:36px;">$1</li></ul>') // <li>
-    toHTML = toHTML.replace(/^ {8,}- (.*$)/gim, '<ul><li style="margin-left:48px;">$1</li></ul>') // <li>
-    toHTML = toHTML.replace(/(^[0-9])+\.\s*(.*$)/gim, '<ol><li style="list-style-type: &quot;$1. &quot;;">$2</li></ol>') // <li>
+    toHTML = toHTML.replace(/(?<!<(?:textarea|code)[^>]*>[^<]*)^\*(.*$)(?![^<]*<\/(?:textarea|code)>)/gim, '<ul><li>$1</li></ul>') // <li>
+    toHTML = toHTML.replace(/(?<!<(?:textarea|code)[^>]*>[^<]*)^- (.*$)(?![^<]*<\/(?:textarea|code)>)/gim, '<ul><li>$1</li></ul>') // <li>
+    toHTML = toHTML.replace(/(?<!<(?:textarea|code)[^>]*>[^<]*)^ {2}- (.*$)(?![^<]*<\/(?:textarea|code)>)/gim, '<ul><li style="margin-left:12px;">$1</li></ul>') // <li>
+    toHTML = toHTML.replace(/(?<!<(?:textarea|code)[^>]*>[^<]*)^ {4}- (.*$)(?![^<]*<\/(?:textarea|code)>)/gim, '<ul><li style="margin-left:24px;">$1</li></ul>') // <li>
+    toHTML = toHTML.replace(/(?<!<(?:textarea|code)[^>]*>[^<]*)^ {6}- (.*$)(?![^<]*<\/(?:textarea|code)>)/gim, '<ul><li style="margin-left:36px;">$1</li></ul>') // <li>
+    toHTML = toHTML.replace(/(?<!<(?:textarea|code)[^>]*>[^<]*)^ {8,}- (.*$)(?![^<]*<\/(?:textarea|code)>)/gim, '<ul><li style="margin-left:48px;">$1</li></ul>') // <li>
+    toHTML = toHTML.replace(/(?<!<(?:textarea|code)[^>]*>[^<]*)(^[0-9])+\.\s*(.*$)(?![^<]*<\/(?:textarea|code)>)/gim, '<ol><li style="list-style-type: &quot;$1. &quot;;">$2</li></ol>') // <li>
 
     toHTML = toHTML.replace(/\\\*/g, '*') //replace /* & \* to *
+    toHTML = toHTML.replace(/\\\~/g, '~') //replace /* & \* to *
+    toHTML = toHTML.replace(/\\\_/g, '_') //replace /* & \* to *
 
     toHTML = toHTML.replace(/\[\x\]/gim, '<input type="checkbox" class="checkboxBox" checked/>')
     toHTML = toHTML.replace(/\[ \]/gim, '<input type="checkbox" class="checkboxBox"/>')
 
-    toHTML = toHTML.replace(/(?<!<textarea[^>]*>[^<]*)(^#{7,} (.*))(?![^<]*<\/textarea>)/gim, '<h6>$2</h6>') // h7+ tag
-    toHTML = toHTML.replace(/(?<!<textarea[^>]*>[^<]*)(^#{6} (.*))(?![^<]*<\/textarea>)/gim, '<h6>$2</h6>') // h6 tag
-    toHTML = toHTML.replace(/(?<!<textarea[^>]*>[^<]*)(^#{5} (.*))(?![^<]*<\/textarea>)/gim, '<h5>$2</h5>') // h5 tag
-    toHTML = toHTML.replace(/(?<!<textarea[^>]*>[^<]*)(^#{4} (.*))(?![^<]*<\/textarea>)/gim, '<h4>$2</h4>') // h4 tag
-    toHTML = toHTML.replace(/(?<!<textarea[^>]*>[^<]*)(^#{3} (.*))(?![^<]*<\/textarea>)/gim, '<h3>$2</h3>') // h3 tag
-    toHTML = toHTML.replace(/(?<!<textarea[^>]*>[^<]*)(^##{2} (.*))(?![^<]*<\/textarea>)/gim, '<h2>$2</h2>') // h2 tag
-    toHTML = toHTML.replace(/(?<!<textarea[^>]*>[^<]*)(^# (.*))(?![^<]*<\/textarea>)/gim, '<hr style="margin-top:50px;margin-bottom:20px"><h1>$2</h1>') // h1 tag
+    toHTML = toHTML.replace(/(?<!<(?:textarea|code)[^>]*>[^<]*)(^#{7,} (.*))(?![^<]*<\/(?:textarea|code)>)/gim, '<h6>$2</h6>') // h7+ tag
+    toHTML = toHTML.replace(/(?<!<(?:textarea|code)[^>]*>[^<]*)(^#{6} (.*))(?![^<]*<\/(?:textarea|code)>)/gim, '<h6>$2</h6>') // h6 tag
+    toHTML = toHTML.replace(/(?<!<(?:textarea|code)[^>]*>[^<]*)(^#{5} (.*))(?![^<]*<\/(?:textarea|code)>)/gim, '<h5>$2</h5>') // h5 tag
+    toHTML = toHTML.replace(/(?<!<(?:textarea|code)[^>]*>[^<]*)(^#{4} (.*))(?![^<]*<\/(?:textarea|code)>)/gim, '<h4>$2</h4>') // h4 tag
+    toHTML = toHTML.replace(/(?<!<(?:textarea|code)[^>]*>[^<]*)(^#{3} (.*))(?![^<]*<\/(?:textarea|code)>)/gim, '<h3>$2</h3>') // h3 tag
+    toHTML = toHTML.replace(/(?<!<(?:textarea|code)[^>]*>[^<]*)(^##{2} (.*))(?![^<]*<\/(?:textarea|code)>)/gim, '<h2>$2</h2>') // h2 tag
+    toHTML = toHTML.replace(/(?<!<(?:textarea|code)[^>]*>[^<]*)(^# (.*))(?![^<]*<\/(?:textarea|code)>)/gim, '<hr style="margin-top:50px;margin-bottom:20px"><h1>$2</h1>') // h1 tag
 
     //toHTML = toHTML.replace(/(?![^<]*>|[^>]*<\/)(.+)(?![^<]*>|[^>]*<\/.)/gim, '<p>$1</p>') // text p balise
     
