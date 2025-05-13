8436d4b2b0c439ce3dcc34da289758bfb6b3bee4
lx78WyY0J5 <9j2k37st@gmail.com>
2025-05-13 15:01:20 +0200

    Another test

--- src/js/include/index.js
+++ src/js/include/index.js
@@ -4,6 +4,7 @@ startTime("index");
 index();
 
 async function index() {
+    await include_html("/src/html/content/og-meta-test.html", "head", false);
     console.log("Loading website");
     await Metadata();
     await pages();
