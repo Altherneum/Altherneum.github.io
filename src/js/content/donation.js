4f57191d1d1bfdee83c44d32c33d6f744f2bcecb
lx78WyY0J5 <9j2k37st@gmail.com>
2025-11-08 00:52:14 +0100

    updated to match a new API

--- src/js/content/donation.js
+++ src/js/content/donation.js
@@ -1,8 +1,6 @@
 main();
 
 async function main() {
-    
-
     var deviseType = "USD";
 
     setTokenData("BTC", deviseType);
@@ -14,22 +12,33 @@ async function main() {
 }
 
 async function setTokenData(token, deviseType) {
-    var price = await getPrice(token, deviseType);
+    var price = await getPriceBinance(token, deviseType);
     var priceFormat = new Intl.NumberFormat('fr-FR', { style: 'currency', currency: deviseType });
 
     setData(token, priceFormat.format(price));
 }
 
-function setData(id, data, deviseType) {
+function setData(id, data) {
     var elem = document.getElementById("prix-" + id);
     elem.textContent = data;
 }
 
-async function getPrice(token, deviseType) {
+async function getPriceBlockChainAPI(token, deviseType) {
     var x = await gather('https://api.blockchain.com/v3/exchange/tickers/' + token + "-" + deviseType);
     var price = getValue(x, "last_trade_price");
     if(price !== undefined){
        return getValue(x, "last_trade_price");
+    }
+    else {
+        return "...";
+    }
+}
+
+async function getPriceBinance(token, deviseType) {
+    var x = await gather("https://api.binance.com/api/v3/ticker/price?symbol=" + token + deviseType);
+    var price = getValue(x, "price");
+    if(price !== undefined){
+       return getValue(x, "price");
     }
     else {
         return "...";
