b290bbb603e2eaed5ec1eab47c65708ea18cafcf
lx78WyY0J5 <9j2k37st@gmail.com>
2025-12-15 08:12:31 +0100

    small fix

--- src/js/include/theme.js
+++ src/js/include/theme.js
@@ -2,11 +2,11 @@ toggleTheme();
 toggleBackgroundGride();
 
 function setTheme(themeName) {
-    localStorage.setItem('theme', themeName);
+    localStorage.setItem("theme", themeName);
     document.documentElement.className = themeName;
     console.log("Loading theme : " + themeName);
     setTransparencyPower();
-    setIconTheme();
+    //setIconTheme(); //already done in the index
 }
 
 function toggleBackgroundGride(){
@@ -20,66 +20,63 @@ function toggleBackgroundGride(){
 }
 
 function toggleTheme() {
-    if (localStorage.getItem('theme') === 'dark') {
-        setTheme('dark');
+    if (localStorage.getItem("theme") === "dark") {
+        setTheme("dark");
     }
-    else if (localStorage.getItem('theme') === 'day') {
-        setTheme('day');
+    else if (localStorage.getItem("theme") === "day") {
+        setTheme("day");
     }
-    else if (localStorage.getItem('theme') === 'light') {
-        setTheme('light');
+    else if (localStorage.getItem("theme") === "light") {
+        setTheme("light");
     }
-    else if (localStorage.getItem('theme') === 'transparent') {
-        setTheme('transparent');
+    else if (localStorage.getItem("theme") === "transparent") {
+        setTheme("transparent");
     }
 
     else {
         const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
         if (systemSettingDark) {
-            setTheme('dark');
+            setTheme("dark");
             console.log("Default color scheme : Dark");
         }
         else {
-            setTheme('light');
+            setTheme("light");
             console.log("Default color scheme : Light");
         }
     }
 }
 
 function changeTheme() {
-    if (localStorage.getItem('theme') === 'day' && localStorage.getItem('ThemeTransparent') === "true") {
-        setTheme('transparent');
+    if (localStorage.getItem("theme") === "day" && localStorage.getItem("ThemeTransparent") === "true") {
+        setTheme("transparent");
     }
-    else if (localStorage.getItem('theme') === 'day') {
-        setTheme('dark');
+    else if (localStorage.getItem("theme") === "day") {
+        setTheme("dark");
     }
-    else if (localStorage.getItem('theme') === 'dark') {
-        setTheme('light');
+    else if (localStorage.getItem("theme") === "dark") {
+        setTheme("light");
     }
-    else if (localStorage.getItem('theme') === 'light') {
-        setTheme('day');
-    }
-    else if (localStorage.getItem('theme') === 'day') {
-        setTheme('dark');
+    else if (localStorage.getItem("theme") === "light") {
+        setTheme("day");
     }
 
-    else { setTheme('dark'); }
+    else { setTheme("dark"); }
 }
 
 async function setIconTheme() {
-    var themeButton = document.getElementById('theme');
+    var themeButton = document.getElementById("theme");
     var img = themeButton.children[0];
 
-    if (localStorage.getItem('theme') === 'dark') {
+    if (localStorage.getItem("theme") === "dark") {
         img.src = "/assets/svg/sun.svg";
     }
-    else if (localStorage.getItem('theme') === 'day') {
+    else if (localStorage.getItem("theme") === "day") {
         img.src = "/assets/svg/light.svg";
     }
-    else if (localStorage.getItem('theme') === 'light') {
+    else if (localStorage.getItem("theme") === "light") {
         img.src = "/assets/svg/moon.svg";
     }
-    else if (localStorage.getItem('theme') === 'transparent') {
+    else if (localStorage.getItem("theme") === "transparent") {
         img.src = "/assets/svg/linux.svg";
     }
 
@@ -87,21 +84,26 @@ async function setIconTheme() {
 }
 
 function setTransparencyPower(){
-    let root = document.querySelector(':root');
+    console.log("Loading transparency module");
+
+    let root = document.querySelector(":root");
     let transparencyPower = localStorage.getItem("TransparencyPower");
     let rootStyle = getComputedStyle(root);
 
-    let R = rootStyle.getPropertyValue('--background-color-R');
-    let G = rootStyle.getPropertyValue('--background-color-G');
-    let B = rootStyle.getPropertyValue('--background-color-B');
+    let R = rootStyle.getPropertyValue("--background-color-R");
+    let G = rootStyle.getPropertyValue("--background-color-G");
+    let B = rootStyle.getPropertyValue("--background-color-B");
     
-    if(localStorage.getItem("ThemeTransparent") === "true"){
-        console.log(rootStyle.getPropertyValue('--background-color') + " to " + transparencyPower);
+    if(localStorage.getItem("ThemeTransparent") === "true" && localStorage.getItem("theme") === "transparent"){
+        console.log("Transparent module ON");
+        console.log(rootStyle.getPropertyValue("--background-color") + " to " + transparencyPower);
         document.documentElement.style.cssText = "--background-color: rgba("+ R + ", " + G + ", " + B + ", 0." + transparencyPower + ")";
     }
     else{
-        if (localStorage.getItem('theme') === 'transparent') {
+        console.log("No transparency settings");
+        if (localStorage.getItem("theme") === "transparent") {
             changeTheme();
+            console.log("Theme is transparent, changeTheme");
         }
         document.documentElement.style.cssText = "--background-color: rgba("+ R + ", " + G + ", " + B + ", 1)";
     }
